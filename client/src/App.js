0cf04b7a6408e913604c1c7162fa91565a21353b
lwx <l@w.x>
2020-05-31 14:44:38 +0800

    POC out. Todo: Add in all the correct mappings.
    API: value => output in code 39.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Slim = styled.div`
+  width: 1px;
+  height: 30px;
+  background-color: black;
+  margin-right: 1px;
+`
+
+const Fat = styled.div`
+  width: 3px;
+  height: 30px;
+  background-color: black;
+  margin-right: 1px;
+`
+
+const Blank = styled.div`
+  width: 2px;
+  height: 100px;
+  background-color: white;
+`
+
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+
+const mapping = {
+  0: Slim,
+  1: Fat,
+  2: Blank
 }
 
-export default App;
+const exampleSequece = [0, 0, 2, 0, 1, 1]
+
+const mapToBlocks = (sequence) => {
+  /**
+   * 0: slim block
+   * 1: fat block
+   * 2: blank
+   * @params sequence: Array
+   */
+  return sequence.map((i, idx) => React.createElement(mapping[i], { key: idx }))
+}
+
+
+const StartStopSequence = () => (
+  <React.Fragment>
+    <Slim />
+    <Blank />
+    <Slim />
+    <Fat />
+    <Fat />
+    <Slim />
+  </React.Fragment>
+)
+
+const App = () => {
+
+  return (
+    <div>
+      App
+      <hr />
+      <Container>
+        <StartStopSequence />
+        {
+          mapToBlocks(exampleSequece)
+        }
+        <StartStopSequence />
+      </Container>
+    </div>
+  )
+}
+
+export default App
